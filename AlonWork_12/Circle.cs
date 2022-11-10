0be7d8cf4439eb6f07c9964723012dafd012b3d5
Dmitry <Dmitry@DESKTOP-V8KGT4B>
2022-11-10 23:35:31 +0300

    Добавьте файлы проекта.

---
+++ AlonWork_12/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlonWork_12
+{
+    static class Circle
+    {
+        static public double GetLenght(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Math.PI * r * r;
+        }
+        static public double CheckPoint( double r, double x, double y)
+        {
+            double hypotenuse = Math.Sqrt(x * x + y * y);
+            if (hypotenuse <= r)
+            {
+                return $"Точка принадлежит кругу";
+            }
+            else
+            {
+                return $"Точка НЕ принадлежит кругу";
+            }
+             
+        }
+    }
+}
---
+++ AlonWork_12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlonWork_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус круга = ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Введите значение координаты x = ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Введите значение координаты y = ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            double lenght = Circle.GetLenght(r);
+            Console.WriteLine("Длина окружности = {0}", lenght);
+            Console.WriteLine();
+
+            double square = Circle.GetSquare(r);
+            Console.WriteLine("Площадь круга = {0}", square);
+            Console.WriteLine();
+
+            double point = Circle.CheckPoint(r, x, y);
+            Console.WriteLine("Длина окружности = {0}", square);
+            Console.WriteLine(point);
+
+            Console.ReadKey();
+        }
+    }
+}
